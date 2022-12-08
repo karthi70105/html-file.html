23eb9f640d792216a37256b42d99a8de0777a1fe
karthi70105 <118422006+karthi70105@users.noreply.github.com>
2022-12-08 15:58:15 +0530

    Initial commit

---
+++ java.js/second.css.js
@@ -0,0 +1,17 @@
+console.log("shiva")
+document.title="shiva"
+alert("boring sleeping dharshan");
+
+
+var name="karthi"
+function abron(r){
+        console.log(r)
+        document.write(r)
+      }
+
+abron("karthikeyan")
+
+console.log(name)
+document.write(name)
+
+document.get
